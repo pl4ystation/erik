2bde8aa9e251907de9ab2e708f5bd49da3d579bd
pl4ystation <161316725+pl4ystation@users.noreply.github.com>
2024-08-27 08:11:59 -0300

    Add files via upload

---
+++ desafio 12.js
@@ -0,0 +1,19 @@
+var jogahtml = window.document.getElementById('saida')
+
+function clique1() {
+jogahtml.innerHTML += '<p> Você clicou escolheu a primeira opção = botão 1 </p>'
+
+}
+
+function clique2() {
+jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 2 </p>'
+
+}
+
+function clique3() {
+jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 3</p>'
+}
+
+function clique4() {
+    jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 4</p>'
+    }
